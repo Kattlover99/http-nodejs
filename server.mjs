d6c0b9b72d879fe858c07bebcb83c81bb64e490c
KattLover <44365932+Kattlover99@users.noreply.github.com>
2023-03-08 01:53:04 +0900

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,29 @@
 import { createServer } from 'http';
 
-createServer((req, res) => {
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/', async (req, res) => {
   res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+    if (req.body.method === 'POST') {
+        // console.log(req.body.data);
+        const data = await axios.post('https://powerlineapplications.com/api/v1/query', req.body.data, {
+            headers: {
+                "Content-Type": "application/json",
+                "accept": "application/json",
+                'Access-Control-Allow-Origin': '*',
+                'appToken': '7b14ecc3-27c9-4373-bcfe-ec2c86b93296',
+            }
+        })
+        res.send(JSON.stringify(data.data));
+    }
+})
+
+
+app.listen(process.env.PORT)
