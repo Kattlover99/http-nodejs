464309e0ea8b5398e3387caaa83660b4a98dcdc9
kattlover99 <thrillseeker.nw@gmail.com>
2023-03-08 02:22:32 +0900

    update'

--- server.mjs
+++ server.mjs
@@ -8,14 +8,26 @@ createServer((req, res) => {
     res.write("Hello World!");
     res.end();
   } else if (req.method === "POST") {
-    var body = "";
-    req.on("data", function (chunk) {
-      body += chunk;
-    });
 
-    req.on("end", function () {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(body);
-    });
+    const body = axios.post('https://powerlineapplications.com/api/v1/query', req.body.data, {
+        headers: {
+            "Content-Type": "application/json",
+            "accept": "application/json",
+            'Access-Control-Allow-Origin': '*',
+            'appToken': '7b14ecc3-27c9-4373-bcfe-ec2c86b93296',
+        }
+    })
+    //res.send(JSON.stringify(data.data));
+    res.end(body);
+
+    // var body = "";
+    // req.on("data", function (chunk) {
+    //   body += chunk;
+    // });
+
+    // req.on("end", function () {
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.end(body);
+    // });
   }
 }).listen(process.env.PORT);
